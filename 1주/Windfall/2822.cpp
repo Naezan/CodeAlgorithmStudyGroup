ed2c3bf61edb023fcbfe097b43e8f0d41e3e1b29
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-09-26 14:07:26 +0900

    init commit

---
+++ 1주/Windfall/2822.cpp
@@ -0,0 +1,48 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    
+    int sum = 0;
+    int min;
+    int num;
+    const int ARRAY_SIZE = 8;
+    vector<pair<int, int>> v1;
+    
+    
+    
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cin >> num;
+        v1.push_back(pair<int, int>(i, num));
+    }
+
+    for (int i = 0; i < v1.size(); ++i)
+    {
+        min = i;
+        for (int j = i + 1; j < v1.size(); ++j)
+        {
+            if (v1[min].second < v1[j].second)
+            {
+                pair<int, int> tempPair = v1[min];
+                v1[min] = v1[j];
+                v1[j] = tempPair;
+            }
+        }
+    }
+    
+    for (int i = 0; i < 5; ++i)
+    {
+        sum += v1[i].second;
+    }
+    cout << sum << endl;
+    sort(v1.begin(), v1.begin() + 5);
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << v1[i].first + 1 << " ";
+    }
+    
+}
+
